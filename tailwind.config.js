cb725fdd22d2b68c9a9f5c0a11181ee73fa0dbb2
omair <omirgibreel7@gmail.com>
2022-01-22 09:18:20 -0800

    alpha release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 module.exports = {
   prefix: '',
   purge: {
-    enabled: true,
-    content: [
-      './src/**/*.{html,ts}',
-    ]
+      enabled: process.env.NODE_ENV === 'production',
+      content: [
+          './src/**/*.{html,ts}',
+      ]
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+      extend: {
+          colors: {
+            'primary': '#0073EA',
+            'danger': '#FF641C',
+            'secondary':'#F0F9FF'
+          },
+      },
   },
   variants: {
-    extend: {},
+      extend: {
+          backgroundColor: ['checked'],
+          borderColor: ['checked'],
+      },
   },
-  plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+   plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+  //plugins: [require('tailwind-bootstrap-grid')()],
 };
